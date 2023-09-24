98bb101ad43fc02c37a9d54e9b292e20e5f1b71b
Beluguy <hello@marcusc.me>
2023-09-23 22:46:20 -0700

    updated files in in lab 2

--- 2/task5/datapath/datapath.sv
+++ 2/task5/datapath/datapath.sv
@@ -1,5 +1,5 @@
 //
-
+`timescale 1 ps / 1 ps
 module datapath(input slow_clock, input fast_clock, input resetb,
                 input load_pcard1, input load_pcard2, input load_pcard3,
                 input load_dcard1, input load_dcard2, input load_dcard3,
--- 2/task5/statemachine/statemachine.sv
+++ 2/task5/statemachine/statemachine.sv
@@ -4,90 +4,78 @@ module statemachine(input slow_clock, input resetb,
                     output load_dcard1, output load_dcard2, output load_dcard3,
                     output player_win_light, output dealer_win_light);
 
-// The code describing your state machine will go here.  Remember that
-// a state machine consists of next state logic, output logic, and the 
-// registers that hold the state.  You will want to review your notes from
-// CPEN 211 or equivalent if you have forgotten how to write a state machine.
+    // The code describing your state machine will go here.  Remember that
+    // a state machine consists of next state logic, output logic, and the 
+    // registers that hold the state.  You will want to review your notes from
+    // CPEN 211 or equivalent if you have forgotten how to write a state machine.
 
-//1 initial state (no cards dealt)
-//4 states for dealing first 4 cards
-//1 state for player getting third card
-//1 state for dealer getting third card
-//1 state for game end
-//8 states 0-7 -> 3 bits
-reg [3:0] state;
-wire [3:0] statewire;
-assign statewire = state;
+    //1 (0) initial state (no cards dealt)
+    //4,(1-4) states for dealing first 4 cards
+    //1 (5) state for player getting third card
+    //1 (6) state for dealer getting third card
+    //1 (7) state for game end
+    //8 states 0-7 -> 3 bits
+    reg [3:0] state;
+    wire [3:0] statewire;
+    assign statewire = state;
 
-reg load_pcard1_reg;
-reg load_pcard2_reg;
-reg load_pcard3_reg;
-reg load_dcard1_reg;
-reg load_dcard2_reg;
-reg load_dcard3_reg;
+    reg load_pcard1_reg;
+    reg load_pcard2_reg;
+    reg load_pcard3_reg;
+    reg load_dcard1_reg;
+    reg load_dcard2_reg;
+    reg load_dcard3_reg;
+    assign load_pcard1 = load_pcard1_reg;
+    assign load_pcard2 = load_pcard2_reg;
+    assign load_pcard3 = load_pcard3_reg;
+    assign load_dcard1 = load_dcard1_reg;
+    assign load_dcard2 = load_dcard2_reg;
+    assign load_dcard3 = load_dcard3_reg;
 
-assign load_pcard1 = load_pcard1_reg;
-assign load_pcard2 = load_pcard2_reg;
-assign load_pcard3 = load_pcard3_reg;
-assign load_dcard1 = load_dcard1_reg;
-assign load_dcard2 = load_dcard2_reg;
-assign load_dcard3 = load_dcard3_reg;
+    reg p_win;
+    reg d_win;
+    assign player_win_light = p_win;
+    assign dealer_win_light = d_win;
 
-reg p_win;
-reg d_win;
+    always_comb begin
+        case(statewire)
+            3'd1: {load_pcard1_reg,load_pcard2_reg,load_pcard3_reg,load_dcard1_reg,load_dcard2_reg,load_dcard3_reg} = 6'b100000;
+            3'd2: {load_pcard1_reg,load_pcard2_reg,load_pcard3_reg,load_dcard1_reg,load_dcard2_reg,load_dcard3_reg} = 6'b000100;
+            3'd3: {load_pcard1_reg,load_pcard2_reg,load_pcard3_reg,load_dcard1_reg,load_dcard2_reg,load_dcard3_reg} = 6'b010000;
+            3'd4: {load_pcard1_reg,load_pcard2_reg,load_pcard3_reg,load_dcard1_reg,load_dcard2_reg,load_dcard3_reg} = 6'b000010;
+            3'd5: {load_pcard1_reg,load_pcard2_reg,load_pcard3_reg,load_dcard1_reg,load_dcard2_reg,load_dcard3_reg} = 6'b001000;
+            3'd6: {load_pcard1_reg,load_pcard2_reg,load_pcard3_reg,load_dcard1_reg,load_dcard2_reg,load_dcard3_reg} = 6'b000001;
+            default: {load_pcard1_reg,load_pcard2_reg,load_pcard3_reg,load_dcard1_reg,load_dcard2_reg,load_dcard3_reg} = 6'b000000;
+        endcase
 
-assign player_win_light = p_win;
-assign dealer_win_light = d_win;
+        if (statewire == 3'd7 && (pscore > dscore)) {p_win,d_win} = 2'b10;
+        else if (statewire == 3'd7 && (pscore < dscore)) {p_win,d_win} = 2'b01;
+        else {p_win,d_win} = 2'b00;
+    end
 
-always_comb begin
+    always_ff @(posedge slow_clock) begin
+        if(resetb == 1'd0) begin
+            state <= 3'd0;   // reset
+        end
 
-case(statewire)
-3'b001: {load_pcard1_reg,load_pcard2_reg,load_pcard3_reg,load_dcard1_reg,load_dcard2_reg,load_dcard3_reg} = 6'b100000;
-3'b010: {load_pcard1_reg,load_pcard2_reg,load_pcard3_reg,load_dcard1_reg,load_dcard2_reg,load_dcard3_reg} = 6'b000100;
-3'b011: {load_pcard1_reg,load_pcard2_reg,load_pcard3_reg,load_dcard1_reg,load_dcard2_reg,load_dcard3_reg} = 6'b010000;
-3'b100: {load_pcard1_reg,load_pcard2_reg,load_pcard3_reg,load_dcard1_reg,load_dcard2_reg,load_dcard3_reg} = 6'b000010;
-3'b101: {load_pcard1_reg,load_pcard2_reg,load_pcard3_reg,load_dcard1_reg,load_dcard2_reg,load_dcard3_reg} = 6'b001000;
-3'b110: {load_pcard1_reg,load_pcard2_reg,load_pcard3_reg,load_dcard1_reg,load_dcard2_reg,load_dcard3_reg} = 6'b000001;
-default: {load_pcard1_reg,load_pcard2_reg,load_pcard3_reg,load_dcard1_reg,load_dcard2_reg,load_dcard3_reg} = 6'b000000;
-endcase
+        else if(statewire < 3'd4) state <= statewire + 3'b001;  // state go from 0 -> 4
 
-if (statewire == 3'b111 && (pscore > dscore)) {p_win,d_win} = 2'b10;
-else if (statewire == 3'b111 && (pscore < dscore)) {p_win,d_win} = 2'b01;
-else {p_win,d_win} = 2'b00;
+        else if((statewire == 3'd4) && ((dscore >= 4'd8) || (pscore >= 4'd8))) state <= 3'd7;  // if pscore or dscore reach >= 8, then state go from 4 -> 7 (natural)
+        else if((statewire == 3'd4) && (pscore <= 4'd5)) state <= 3'd5;				           // if pscore is <= 4 -> 5 (pscore <= 5)
+        else if((statewire == 3'd4) && (pscore > 4'd5) && (dscore <= 4'd5)) state <= 3'd6;     // 4 -> 6 (pscore > 5 and dscore <= 5)
+        else if((statewire == 3'd4) && (pscore > 4'd5) && (dscore > 4'd5)) state <= 3'd7;      // 4- > 7 (pscore > 5 and dscore > 5)
 
+        else if((statewire == 3'd5) && (dscore == 4'd7)) state <= 3'd7;							// 5 -> 7 (dscore  = 7)
+        else if((statewire == 3'd5) && (dscore == 4'd6) && (pcard3 >= 4'd6) && (pcard3 <= 4'd7)) state <= 3'd6;    // 5 -> 6 (dscore = 6, pcard3 between 6,7)
+        else if((statewire == 3'd5) && (dscore == 4'd6) && ~((pcard3 >= 4'd6) && (pcard3 <= 4'd7))) state <= 3'd7; // 5 -> 7 (dscore = 6, pcard3 not between 6,7)
+        else if((statewire == 3'd5) && (dscore == 4'd5) && (pcard3 >= 4'd4) && (pcard3 <= 4'd7)) state <= 3'd6;    // 5 -> 6 (dscore = 5, pcard3 between 4,7)
+        else if((statewire == 3'd5) && (dscore == 4'd5) && ~((pcard3 >= 4'd4) && (pcard3 <= 4'd7))) state <= 3'd7; // 5 -> 7 (dscore = 5, pcard3 not between 4,7)
+        else if((statewire == 3'd5) && (dscore == 4'd4) && (pcard3 >= 4'd2) && (pcard3 <= 4'd7)) state <= 3'd6;    // 5 -> 6 (dscore = 4, pcard3 between 2,7)
+        else if((statewire == 3'd5) && (dscore == 4'd4) && ~((pcard3 >= 4'd2) && (pcard3 <= 4'd7))) state <= 3'd7; // 5 -> 7 (dscore = 4, pcard3 not between 2,7)
+        else if((statewire == 3'd5) && (dscore == 4'd3) && (pcard3 != 4'd8)) state <= 3'd6;    // 5 -> 6 (dscore = 3, pcard3 not 8)
+        else if((statewire == 3'd5) && (dscore == 4'd3) && ~(pcard3 != 4'd8)) state <= 3'd7;   // 5 -> 7 (dscore = 3, pcard3 is 8)
+        else if((statewire == 3'd5) && (dscore <= 4'd2)) state <= 3'd6;			 // 5 -> 6 (dscore <= 2)
 
-end
-
-always_ff @(posedge slow_clock) begin
-if(resetb == 1'b0) begin
-state <= 3'b000;   // reset
-end
-
-else if(statewire < 3'b100) state <= statewire + 3'b001;  // 0 -> 4
-
-else if((statewire == 3'b100) && ((dscore >= 4'b1000) || (pscore >= 4'b1000))) state <= 3'b111;  // 4 -> 7 (natural)
-else if((statewire == 3'b100) && (pscore <= 4'b0101)) state <= 3'b101;				 // 4 -> 5 (pscore <= 5)
-else if((statewire == 3'b100) && (pscore > 4'b0101) && (dscore <= 4'b0101)) state <= 3'b110;     // 4 -> 6 (pscore > 5 and dscore <= 5)
-else if((statewire == 3'b100) && (pscore > 4'b0101) && (dscore > 4'b0101)) state <= 3'b111;      // 4- > 7 (pscore > 5 and dscore > 5)
-
-else if((statewire == 3'b101) && (dscore == 4'b0111)) state <= 3'b111;							// 5 -> 7 (dscore  = 7)
-else if((statewire == 3'b101) && (dscore == 4'b0110) && (pcard3 >= 4'b0110) && (pcard3 <= 4'b0111)) state <= 3'b110;    // 5 -> 6 (dscore = 6, pcard3 between 6,7)
-else if((statewire == 3'b101) && (dscore == 4'b0110) && ~((pcard3 >= 4'b0110) && (pcard3 <= 4'b0111))) state <= 3'b111; // 5 -> 7 (dscore = 6, pcard3 not between 6,7)
-else if((statewire == 3'b101) && (dscore == 4'b0101) && (pcard3 >= 4'b0100) && (pcard3 <= 4'b0111)) state <= 3'b110;    // 5 -> 6 (dscore = 5, pcard3 between 4,7)
-else if((statewire == 3'b101) && (dscore == 4'b0101) && ~((pcard3 >= 4'b0100) && (pcard3 <= 4'b0111))) state <= 3'b111; // 5 -> 7 (dscore = 5, pcard3 not between 4,7)
-else if((statewire == 3'b101) && (dscore == 4'b0100) && (pcard3 >= 4'b0010) && (pcard3 <= 4'b0111)) state <= 3'b110;    // 5 -> 6 (dscore = 4, pcard3 between 2,7)
-else if((statewire == 3'b101) && (dscore == 4'b0100) && ~((pcard3 >= 4'b0010) && (pcard3 <= 4'b0111))) state <= 3'b111; // 5 -> 7 (dscore = 4, pcard3 not between 2,7)
-else if((statewire == 3'b101) && (dscore == 4'b0011) && (pcard3 != 4'b1000)) state <= 3'b110;    // 5 -> 6 (dscore = 3, pcard3 not 8)
-else if((statewire == 3'b101) && (dscore == 4'b0011) && ~(pcard3 != 4'b1000)) state <= 3'b111;   // 5 -> 7 (dscore = 3, pcard3 is 8)
-else if((statewire == 3'b101) && (dscore <= 4'b0010)) state <= 3'b110;			 // 5 -> 6 (dscore <= 2)
-
-else if(statewire == 3'b110) state <= 3'b111;		    // 6 -> 7
-
-
-
-
-
-end
-
+        else if(statewire == 3'd6) state <= 3'd7;		    // 6 -> 7
+    end
 endmodule
-
