e1f4284053394ddde44ca9ac5bf3e3d471afb6d3
Beluguy <hello@marcusc.me>
2023-09-26 09:29:39 -0700

    done datapath

--- 2/task5/datapath/datapath.sv
+++ 2/task5/datapath/datapath.sv
@@ -1,5 +1,4 @@
-//
-`timescale 1 ps / 1 ps
+// datapath connect everything and keep track of scores
 module datapath(input slow_clock, input fast_clock, input resetb,
                 input load_pcard1, input load_pcard2, input load_pcard3,
                 input load_dcard1, input load_dcard2, input load_dcard3,
@@ -7,7 +6,7 @@ module datapath(input slow_clock, input fast_clock, input resetb,
                 output [3:0] pscore_out, output [3:0] dscore_out,
                 output[6:0] HEX5, output[6:0] HEX4, output[6:0] HEX3,
                 output[6:0] HEX2, output[6:0] HEX1, output[6:0] HEX0);
-						
+
     // The code describing your datapath will go here.  Your datapath 
     // will hierarchically instantiate six card7seg blocks, two scorehand
     // blocks, and a dealcard block.  The registers may either be instatiated
@@ -15,29 +14,39 @@ module datapath(input slow_clock, input fast_clock, input resetb,
     //
     // Follow the block diagram in the Lab 1 handout closely as you write this code.
 
-    reg pcard1_reg;
-    reg pcard2_reg;
-    reg pcard3_reg;
-    reg dcard1_reg;
-    reg dcard2_reg;
-    reg dcard3_reg;
+    assign pcard3_out = pcard3_reg;
 
-    card7seg c1 (, [6:0]HEX1);
-    card7seg c2 ();
-    card7seg c3 ();
-    card7seg c4 ();
-    card7seg c5 ();
-    card7seg c6 ();
+    // instantiation of the 6 reg that store the player's and dealer's card
+    reg [3:0] pcard1_reg;
+    reg [3:0] pcard2_reg;
+    reg [3:0] pcard3_reg;
+    reg [3:0] dcard1_reg;
+    reg [3:0] dcard2_reg;
+    reg [3:0] dcard3_reg;
 
-    scorehand sh1();
-    scorehand sh2();
+    // instantiation of the 6 block that drives the HEX display
+    card7seg c1 (.SW(pcard1_reg), .HEX0(HEX0));
+    card7seg c2 (.SW(pcard2_reg), .HEX0(HEX1));
+    card7seg c3 (.SW(pcard3_reg), .HEX0(HEX2));
+    card7seg c4 (.SW(dcard4_reg), .HEX0(HEX3));
+    card7seg c5 (.SW(dcard5_reg), .HEX0(HEX4));
+    card7seg c6 (.SW(dcard6_reg), .HEX0(HEX5));
 
-    dealcard dc(.clock(fast_clock), .resetb(resetb), .new_card());
+    // instantiation of the 2 blocks that computes the value of cards from players and dealers respectively
+    scorehand shp(.card1(pcard1_reg), .card2(pcard2_reg), .card3(pcard3_reg), .total(pscore_out));
+    scorehand shd(.card1(dcard1_reg), .card2(dcard2_reg), .card3(dcard3_reg), .total(dscore_out));
 
+    // instantiation of the block that deal random cards to player and dealer
+    wire [3:0] new_card;
+    dealcard dc(.clock(fast_clock), .resetb(resetb), .new_card(new_card));
 
-    always_comb
-
-
-
+    // for the registers
+    always_ff @(posedge slow_clock) begin
+        if (load_pcard1 == 1 && resetb != 0) pcard1_reg <= new_card;
+        else if (load_pcard2 == 1 && resetb != 0) pcard2_reg <= new_card;
+        else if (load_pcard3 == 1 && resetb != 0) pcard3_reg <= new_card;
+        else if (load_dcard1 == 1 && resetb != 0) dcard1_reg <= new_card;
+        else if (load_dcard2 == 1 && resetb != 0) dcard2_reg <= new_card;
+        else if (load_dcard3 == 1 && resetb != 0) dcard3_reg <= new_card;
+    end 
 endmodule
-
--- 2/task5/datapath/tb_datapath.sv
+++ 2/task5/datapath/tb_datapath.sv
@@ -1,8 +1,66 @@
+`timescale 1 ps / 1 ps
 module tb_datapath();
-
 // Your testbench goes here. Make sure your tests exercise the entire design
 // in the .sv file.  Note that in our tests the simulator will exit after
 // 10,000 ticks (equivalent to "initial #10000 $finish();").
-						
+    integer failed = 0;
+    integer passed = 0;
+
+    reg slow_clock;
+    reg fast_clock;
+	reg resetb; 
+
+    reg load_pcard1;
+	reg load_pcard2;
+	reg load_pcard3;
+	reg load_dcard1;
+	reg load_dcard2;
+	reg load_dcard3;
+
+	wire [3:0] pscore;
+	wire [3:0] dscore;
+	wire [3:0] pcard3;
+
+    wire [6:0] HEX0;
+    wire [6:0] HEX1;
+    wire [6:0] HEX2;
+    wire [6:0] HEX3;
+    wire [6:0] HEX4;
+    wire [6:0] HEX5;
+
+    datapath dut (.slow_clock(slow_clock), .fast_clock(fast_clock), input resetb,
+                .load_pcard1(load_pcard1), .load_pcard2(load_pcard2), .load_pcard3(load_pcard3),
+                .load_dcard1(load_dcard1), .load_dcard2(load_dcard2), .load_dcard3(load_dcard3),
+                .pcard3_out (pcard3), .pscore_out(pscore), .dscore_out(dscore),
+                .HEX5(), .HEX4(), .HEX3(), .HEX2(), .HEX1(), .HEX0());
+
+    task s_clk;
+		begin
+		#2;
+		slow_clock = 1'b1;
+		#2;
+		slow_clock = 1'b0;
+		#2;
+		end
+	endtask
+
+    task f_clk;
+		begin
+		#1;
+		fast_clock = 1'b1;
+		#1;
+		fast_clock = 1'b0;
+		#1;
+		end
+	endtask
+
+	initial begin
+
+
+
+    end
+    $display("Total number of tests failed is: %d", failed);
+    $display("Total number of tests passed is: %d", passed);
+    $stop;	
 endmodule
 
