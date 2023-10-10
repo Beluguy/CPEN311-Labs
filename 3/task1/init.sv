c0b5f86a7a3a4622a9e42b4683c82a251c108f60
NissanFairladyZ <117058399+NissanFairladyZ@users.noreply.github.com>
2023-10-10 10:21:07 -0700

    overwrite

--- 3/task1/init.sv
+++ 3/task1/init.sv
@@ -1,21 +1,44 @@
-// callee
 module init(input logic clk, input logic rst_n,
             input logic en, output logic rdy,
             output logic [7:0] addr, output logic [7:0] wrdata, output logic wren);
 
-    integer i, j;
+// your code here
 
-    always_ff@(posedge clk) begin
-        if (rst_n == 1'd1) rdy = 1'd1;
-        else rdy = 1'd0;
-    end
+integer i,j;
+reg initialized;
 
-    always_ff @(posedge (!rst_n && en)) begin
-        rdy = 1'd0;
-        wren = 1'd1;
-        for (i = 1; i < 255; i = i + 1) begin
-            addr = i;
-            wrdata = i;
-        end
-    end 
+
+//wren = 1'd1;
+//for(i=0; i<255; i = i+1) begin
+     //       addr = i;
+    //        wrdata = i;
+   //     end
+
+
+
+
+
+
+always_ff @(posedge(!rst_n)) begin
+        initialized <= 1'b0;
+end
+
+always_ff @(posedge(clk)) begin
+        if(rdy && en && !initialized) begin
+		rdy <= 1'b0;
+                wren <= 1'b1;
+                for(i=0; i<255; i = i+1) begin
+                     addr <= i;
+                     wrdata <= i;
+                end   
+                initialized <= 1'b1;
+	        rdy <= 1'b1;
+                wren <= 1'b0;
+	end
+        else begin
+                rdy <= 1'b1;
+                wren <= 1'b0;
+	end
+end
+
 endmodule: init
