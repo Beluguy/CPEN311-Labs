af5f8b6bc3f74657c8c77ee2c1b701ba16cef6ae
Beluguy <hello@marcusc.me>
2023-10-10 09:21:58 -0700

    updated task1

--- 3/task1/init.sv
+++ 3/task1/init.sv
@@ -4,7 +4,12 @@ module init(input logic clk, input logic rst_n,
             output logic [7:0] addr, output logic [7:0] wrdata, output logic wren);
 
     integer i, j;
-    
+
+    always_ff@(posedge clk) begin
+        if (rstn_n == 0'd1) rdy = 1'd1;
+        else rdy = 0'd1;
+    end
+
     always_ff @(posedge (!rst_n && en)) begin
         rdy = 0'd0;
         wren = 1'd1;
--- 3/task1/task1.sv
+++ 3/task1/task1.sv
@@ -4,15 +4,14 @@ module task1(input logic CLOCK_50, input logic [3:0] KEY, input logic [9:0] SW,
              output logic [6:0] HEX3, output logic [6:0] HEX4, output logic [6:0] HEX5,
              output logic [9:0] LEDR);
 
-
     wire en, rdy, wren;
     wire [7:0] addr;
     wire [7:0] wrdata;
+    wire [7:0] q;
 
-    s_mem s(.address(addr), .clock(CLOCK_50), .data(wrdata), .wren(wren), .q());
+    s_mem s(.address(addr), .clock(CLOCK_50), .data(wrdata), .wren(wren), .q(q));
+    init init(.clk(CLOCK_50), .rst_n(KEY[3]), .en(en), .rdy(rdy), .addr(addr), .wrdata(wrdata), .wren(wren));
 
-    init init(.clk(CLOCK_50), .rst_n(KEY[3]), .en(), .rdy(), .addr(addr), .wrdata(wrdata), .wren(wren));
-
-    // your code here
+    always_ff
 
 endmodule: task1
