b9abd8a4f28fc616ee12dd5a0d393ea4b146db34
Beluguy <hello@marcusc.me>
2023-10-10 09:01:00 -0700

    updated task1

--- 3/task1/init.sv
+++ 3/task1/init.sv
@@ -1,11 +1,12 @@
+// callee
 module init(input logic clk, input logic rst_n,
             input logic en, output logic rdy,
             output logic [7:0] addr, output logic [7:0] wrdata, output logic wren);
 
     integer i, j;
-    //s_men s(.address(addr), .clock(clk), .data(wrdata), .wren(wren), .q());
-
+    
     always_ff @(posedge (!rst_n && en)) begin
+        rdy = 0'd0;
         wren = 1'd1;
         for (i = 0; i < 255; i = i + 1) begin
             addr = i;
--- 3/task1/task1.sv
+++ 3/task1/task1.sv
@@ -1,9 +1,13 @@
+//caller
 module task1(input logic CLOCK_50, input logic [3:0] KEY, input logic [9:0] SW,
              output logic [6:0] HEX0, output logic [6:0] HEX1, output logic [6:0] HEX2,
              output logic [6:0] HEX3, output logic [6:0] HEX4, output logic [6:0] HEX5,
              output logic [9:0] LEDR);
 
-    // your code here
+
+    wire en, rdy, wren;
+    wire [7:0] addr;
+    wire [7:0] wrdata;
 
     s_mem s(.address(addr), .clock(CLOCK_50), .data(wrdata), .wren(wren), .q());
 
