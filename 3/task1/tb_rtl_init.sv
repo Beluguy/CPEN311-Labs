de821c2b36b17abc9d1ac21227f212c7cdfef5ca
NissanFairladyZ <117058399+NissanFairladyZ@users.noreply.github.com>
2023-10-12 14:15:27 -0700

    Update tb_rtl_init.sv

--- 3/task1/tb_rtl_init.sv
+++ 3/task1/tb_rtl_init.sv
@@ -6,62 +6,73 @@ module tb_rtl_init();
     reg clk, rst_n, en, rdy, wren;
     reg [7:0] addr;
 	wire [7:0] wrdata;
+	
 
     init dut (.clk(clk), .rst_n(rst_n), .en(en), .rdy(rdy), .addr(addr), .wrdata(wrdata), .wren(wren));
 
-    task clock;
-		begin
+task clock;
+	begin
 		clk = 1'b1;
-		#2;
-		clk = 1'b0;
-		#2;
-		end
-	endtask
-
-	initial begin
-        rst_n = 1'd0;
-		#2;
-		rst_n = 1'd1;
-
-        $display("Now testing wrdata output");
-	
-        for (i = 0; i < 256; i = i + 1) begin
-            if (dut.rdy == 1'd1) en = 1'd1;
-            else en = 1'd0;
-            clock;
-	    	if(i == 0) begin
-				if((dut.wrdata != 0) || (dut.addr != 0)) begin
-					$error("wrdata = %d, addr = %d, they should be %d", wrdata, addr, i);
-					failed++;
-				end 
-				else begin
-					$display("Correct: wrdata output = %d", wrdata);
-				end
-            end
-           
-			else if(i > 0) begin
-				if((dut.wrdata != i - 1) || (dut.addr != i - 1)) begin
-					$error("wrdata = %d, addr = %d, they should be %d", wrdata, addr, i - 1);
-					failed++;
-				end 
-				else begin
-					$display("Correct: wrdata output = %d", wrdata);
-				end
-			end
-       
-	 end 
-        $display("Total number of tests failed is: %d", failed);
-        
-		clock;
-		clock;
-		en = 1'b1;
-		clock;
-		en = 1'b0;
-			#1;
-			rst_n = 1'd0;     //async reset
-			#1;
-			rst_n = 1'd1;
 		#1;
-		$stop;
-    end 
+		clk = 1'b0;
+		#1;
+		end
+endtask
+
+initial begin
+	rst_n = 1'b0;
+	#1;
+	rst_n = 1'b1;
+	#1;
+	clock;
+	clock;
+	clock;
+	if((addr == 0)&&(wrdata == 0)&&(wren == 0)) begin
+		$display("Correct output for addr, wrdata, wren: %d %d %b", addr, wrdata, wren);
+	end
+	else begin
+		$display("Incorrect output for addr, wrdata, wren: %d %d %b. Expected output : 0, 0, 0", addr, wrdata, wren);
+		failed = failed + 1;
+	end
+	en = 1'b1;
+	#1;
+	for(i = 0; i < 256; i = i + 1) begin
+		clock;
+		if((addr == i)&&(wrdata == i)&&(wren == 1)) begin
+			$display("Correct output for addr, wrdata, wren: %d %d %b", addr, wrdata, wren);
+		end
+		else begin
+			$display("Incorrect output for addr, wrdata, wren: %d %d %b. Expected output : %d, %d, 1", addr, wrdata, wren, i, i);
+		failed = failed + 1;
+		end
+		en = 1'b0;
+	end
+	clock;
+	if((addr == 255)&&(wrdata == 255)&&(wren == 0)) begin
+		$display("Correct output for addr, wrdata, wren: %d %d %b", addr, wrdata, wren);
+	end
+	else begin
+		$display("Incorrect output for addr, wrdata, wren: %d %d %b. Expected output : 255, 255, 0", addr, wrdata, wren);
+		failed = failed + 1;	
+	end
+	clock;
+	clock;
+	en = 1'b1;
+	clock;
+	en = 1'b0;
+	clock;
+	rst_n = 1'b0;
+	#1;
+	rst_n = 1'b1;
+	#1;
+	clock;
+	clock;
+	en = 1'b1;
+	clock;
+	en = 1'b0;
+	$display("Tests failed: %d", failed);
+	$stop;
+end
+
+	
 endmodule: tb_rtl_init
