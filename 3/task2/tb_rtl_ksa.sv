2d70f4eebf1b2581b01f77521ab7fd7ff7324a9a
NissanFairladyZ <117058399+NissanFairladyZ@users.noreply.github.com>
2023-10-12 21:10:40 -0700

    Update tb_rtl_ksa.sv

--- 3/task2/tb_rtl_ksa.sv
+++ 3/task2/tb_rtl_ksa.sv
@@ -1,5 +1,68 @@
+`timescale 1 ps / 1 ps
 module tb_rtl_ksa();
+	integer failed = 0;
+	integer i = 0;
 
-// Your testbench goes here.
+//module ksa(input logic clk, input logic rst_n,
+//           input logic en, output logic rdy,
+//           input logic [23:0] key,
+//           output logic [7:0] addr, input logic [7:0] rddata, output logic [7:0] wrdata, output logic wren);
+
+reg clk;
+reg rst_n;
+reg en;
+wire rdy;
+reg [23:0] key;
+wire [7:0] addr;
+reg [7:0] rddata;
+wire [7:0] wrdata;
+wire wren;
+
+ksa dut(.clk(clk),
+	.rst_n(rst_n),
+	.en(en),
+	.rdy(rdy),
+	.key(key),
+	.addr(addr),
+	.rddata(rddata),
+	.wrdata(wrdata),
+	.wren(wren));
+
+task clock;
+	begin
+		clk = 1'b1;
+		#1;
+		clk = 1'b0;
+		#1;
+		end
+endtask
+
+initial begin
+	rst_n = 1'b0;
+	#1;       
+	rst_n = 1'b1;
+	key = 10'b1100111100;
+	#1;
+	clock;
+	clock;
+	clock;
+	en = 1'b1;
+	
+        for(i = 0; i < 256; i = i + 1) begin
+		clock;     //1
+		en = 1'b0;
+		rddata = 8'd50;
+		clock;
+		clock;
+		clock;
+		clock;
+		clock;
+		clock;
+		clock;
+		clock;
+	end
+	
+
+end
 
 endmodule: tb_rtl_ksa
