7299643ae5189e0d02e47b5fe39748a4d04a4ab3
Beluguy <hello@marcusc.me>
2023-10-17 08:27:09 -0700

    indented task3

--- 3/task3/arc4.sv
+++ 3/task3/arc4.sv
@@ -4,132 +4,132 @@ module arc4(input logic clk, input logic rst_n,
             output logic [7:0] ct_addr, input logic [7:0] ct_rddata,
             output logic [7:0] pt_addr, input logic [7:0] pt_rddata, output logic [7:0] pt_wrdata, output logic pt_wren);
 
-integer state;
+	integer state;
 
-wire [7:0] init_s_addr;
-wire [7:0] init_s_wrdata;
-wire init_s_wren;
+	wire [7:0] init_s_addr;
+	wire [7:0] init_s_wrdata;
+	wire init_s_wren;
 
-wire [7:0] ksa_s_addr;
-wire [7:0] ksa_s_wrdata;
-wire ksa_s_wren;
+	wire [7:0] ksa_s_addr;
+	wire [7:0] ksa_s_wrdata;
+	wire ksa_s_wren;
 
-wire [7:0] prga_s_addr;
-wire [7:0] prga_s_wrdata;
-wire prga_s_wren;
+	wire [7:0] prga_s_addr;
+	wire [7:0] prga_s_wrdata;
+	wire prga_s_wren;
 
-reg [7:0] s_addr_reg;
-reg [7:0] s_wrdata_reg;
-reg s_wren_reg;
+	reg [7:0] s_addr_reg;
+	reg [7:0] s_wrdata_reg;
+	reg s_wren_reg;
 
-wire [7:0] s_addr;
-wire [7:0] s_rddata;
-wire [7:0] s_wrdata;
-wire s_wren;
+	wire [7:0] s_addr;
+	wire [7:0] s_rddata;
+	wire [7:0] s_wrdata;
+	wire s_wren;
 
-assign s_addr = s_addr_reg;
-assign s_wrdata = s_wrdata_reg;
-assign s_wren = s_wren_reg;
+	assign s_addr = s_addr_reg;
+	assign s_wrdata = s_wrdata_reg;
+	assign s_wren = s_wren_reg;
 
-wire init_done;
-wire ksa_done;
-wire prga_done;
+	wire init_done;
+	wire ksa_done;
+	wire prga_done;
 
-reg init_started;
-reg ksa_started;
+	reg init_started;
+	reg ksa_started;
 
-reg ksa_en_reg;
-wire ksa_en;
-assign ksa_en = ksa_en_reg;
+	reg ksa_en_reg;
+	wire ksa_en;
+	assign ksa_en = ksa_en_reg;
 
-reg prga_en_reg;
-wire prga_en;
-assign prga_en = prga_en_reg;
+	reg prga_en_reg;
+	wire prga_en;
+	assign prga_en = prga_en_reg;
 
 
-	s_mem s(.address(s_addr),
-		.clock(clk),
-		.data(s_wrdata),
-		.wren(s_wren),
-		.q(s_rddata));
+		s_mem s(.address(s_addr),
+			.clock(clk),
+			.data(s_wrdata),
+			.wren(s_wren),
+			.q(s_rddata));
 
-	init i(.clk(clk), 
-		.rst_n(rst_n), 
-		.en(en), 
-		.rdy(init_done),
- 
-		.addr(init_s_addr),      //s module
-		.wrdata(init_s_wrdata), 
-		.wren(init_s_wren));
-
-	ksa k(.clk(clk),
-		.rst_n(rst_n),
-		.en(ksa_en),
-		.rdy(ksa_done),
-		.key(key),
-
-		.addr(ksa_s_addr),       //s module
-		.rddata(s_rddata),
-		.wrdata(ksa_s_wrdata),
-		.wren(ksa_s_wren));
-
-	prga p(.clk(clk),
-		.rst_n(rst_n),
-		.en(prga_en),
-		.rdy(prga_done),
-		.key(key),
-
-		.s_addr(prga_s_addr),      //s module
-		.s_rddata(s_rddata),
-		.s_wrdata(prga_s_wrdata),
-		.s_wren(prga_s_wren),
-
-		.ct_addr(ct_addr),
-		.ct_rddata(ct_rddata),
-		.pt_addr(pt_addr),
-		.pt_rddata(pt_rddata),
-		.pt_wrdata(pt_wrdata),
-		.pt_wren(pt_wren));
-
-always_comb begin
-	case({init_started,init_done})
-		2'b11: ksa_en_reg = 1'b1;      //ksa_en only when init has started and is done
-		default: ksa_en_reg = 1'b0;
-	endcase
-
-	case({ksa_started,ksa_done})
-		2'b11: prga_en_reg = 1'b1;     //prga_en only when ksa has started and is done
-		default: prga_en_reg = 1'b0;
-	endcase
+		init i(.clk(clk), 
+			.rst_n(rst_n), 
+			.en(en), 
+			.rdy(init_done),
 	
-	case(state)
-		1: {s_addr_reg,s_wrdata_reg,s_wren_reg} = {init_s_addr, init_s_wrdata, init_s_wren};           // init
-		2: {s_addr_reg,s_wrdata_reg,s_wren_reg} = {ksa_s_addr, ksa_s_wrdata, ksa_s_wren};      // ksa
-		3: {s_addr_reg,s_wrdata_reg,s_wren_reg} = {prga_s_addr, prga_s_wrdata, prga_s_wren};  //prga
-		default: {s_addr_reg,s_wrdata_reg,s_wren_reg} = 17'b0;
-	endcase
+			.addr(init_s_addr),      //s module
+			.wrdata(init_s_wrdata), 
+			.wren(init_s_wren));
 
-	if(prga_done && ksa_done && init_done) rdy = 1'b1; //final output rdy
-	else rdy = 1'b0;
-end
+		ksa k(.clk(clk),
+			.rst_n(rst_n),
+			.en(ksa_en),
+			.rdy(ksa_done),
+			.key(key),
 
-always @(posedge(clk), negedge(rst_n)) begin
-		if(!rst_n) begin
-			state <= 0;
-			init_started <= 1'b0;
-			ksa_started <= 1'b0;
-		end
-		else if((state == 0) && (en == 1'b1)) begin
-			init_started <= 1'b1;
-			state <= state + 1;
-		end
-		else if((state == 1) && (ksa_en == 1'b1)) begin
-			ksa_started <= 1'b1;
-			state <= state + 1;
-		end
-		else if((state == 2) && (prga_en == 1'b1)) begin
-			state <= state + 1;
-		end
-end
+			.addr(ksa_s_addr),       //s module
+			.rddata(s_rddata),
+			.wrdata(ksa_s_wrdata),
+			.wren(ksa_s_wren));
+
+		prga p(.clk(clk),
+			.rst_n(rst_n),
+			.en(prga_en),
+			.rdy(prga_done),
+			.key(key),
+
+			.s_addr(prga_s_addr),      //s module
+			.s_rddata(s_rddata),
+			.s_wrdata(prga_s_wrdata),
+			.s_wren(prga_s_wren),
+
+			.ct_addr(ct_addr),
+			.ct_rddata(ct_rddata),
+			.pt_addr(pt_addr),
+			.pt_rddata(pt_rddata),
+			.pt_wrdata(pt_wrdata),
+			.pt_wren(pt_wren));
+
+	always_comb begin
+		case({init_started,init_done})
+			2'b11: ksa_en_reg = 1'b1;      //ksa_en only when init has started and is done
+			default: ksa_en_reg = 1'b0;
+		endcase
+
+		case({ksa_started,ksa_done})
+			2'b11: prga_en_reg = 1'b1;     //prga_en only when ksa has started and is done
+			default: prga_en_reg = 1'b0;
+		endcase
+		
+		case(state)
+			1: {s_addr_reg,s_wrdata_reg,s_wren_reg} = {init_s_addr, init_s_wrdata, init_s_wren};           // init
+			2: {s_addr_reg,s_wrdata_reg,s_wren_reg} = {ksa_s_addr, ksa_s_wrdata, ksa_s_wren};      // ksa
+			3: {s_addr_reg,s_wrdata_reg,s_wren_reg} = {prga_s_addr, prga_s_wrdata, prga_s_wren};  //prga
+			default: {s_addr_reg,s_wrdata_reg,s_wren_reg} = 17'b0;
+		endcase
+
+		if(prga_done && ksa_done && init_done) rdy = 1'b1; //final output rdy
+		else rdy = 1'b0;
+	end
+
+	always @(posedge(clk), negedge(rst_n)) begin
+			if(!rst_n) begin
+				state <= 0;
+				init_started <= 1'b0;
+				ksa_started <= 1'b0;
+			end
+			else if((state == 0) && (en == 1'b1)) begin
+				init_started <= 1'b1;
+				state <= state + 1;
+			end
+			else if((state == 1) && (ksa_en == 1'b1)) begin
+				ksa_started <= 1'b1;
+				state <= state + 1;
+			end
+			else if((state == 2) && (prga_en == 1'b1)) begin
+				state <= state + 1;
+			end
+	end
 
 endmodule: arc4
--- 3/task3/task3.sv
+++ 3/task3/task3.sv
@@ -3,56 +3,55 @@ module task3(input logic CLOCK_50, input logic [3:0] KEY, input logic [9:0] SW,
              output logic [6:0] HEX3, output logic [6:0] HEX4, output logic [6:0] HEX5,
              output logic [9:0] LEDR);
 
-wire rst_n;
-wire en;
+	wire rst_n;
+	wire en;
 
-assign rst_n = KEY[3];
-assign en = ~KEY[0];
+	assign rst_n = KEY[3];
+	assign en = ~KEY[0];
 
-wire [7:0] ct_addr;
-wire [7:0] ct_rddata;
+	wire [7:0] ct_addr;
+	wire [7:0] ct_rddata;
 
-wire [7:0] pt_addr;
-wire [7:0] pt_rddata;
-wire [7:0] pt_wrdata;
-wire pt_wren;
+	wire [7:0] pt_addr;
+	wire [7:0] pt_rddata;
+	wire [7:0] pt_wrdata;
+	wire pt_wren;
 
-wire rdy;
-wire [23:0] key;
-assign key = {14'b0,SW[9:0]};
+	wire rdy;
+	wire [23:0] key;
+	assign key = {14'b0,SW[9:0]};
 
-wire [7:0] ct_wrdata;
-assign ct_wrdata = 8'b0;
-wire ct_wren;
-assign ct_wren = 1'b0;
-  
-  // your code here
+	wire [7:0] ct_wrdata;
+	assign ct_wrdata = 8'b0;
+	wire ct_wren;
+	assign ct_wren = 1'b0;
+	
+	// your code here
 
-    ct_mem ct(.address(ct_addr),
-		.clock(CLOCK_50),
-		.data(ct_wrdata),
-		.wren(ct_wren),
-		.q(ct_rddata));
+		ct_mem ct(.address(ct_addr),
+			.clock(CLOCK_50),
+			.data(ct_wrdata),
+			.wren(ct_wren),
+			.q(ct_rddata));
 
-    pt_mem pt(.address(pt_addr),
-		.clock(CLOCK_50),
-		.data(pt_wrdata),
-		.wren(pt_wren),
-		.q(pt_rddata));
+		pt_mem pt(.address(pt_addr),
+			.clock(CLOCK_50),
+			.data(pt_wrdata),
+			.wren(pt_wren),
+			.q(pt_rddata));
 
-    arc4 a4(.clk(CLOCK_50),
-		.rst_n(rst_n),
-		.en(en),
-		.rdy(rdy),
-		.key(key),
-		.ct_addr(ct_addr),
-		.ct_rddata(ct_rddata),
-		.pt_addr(pt_addr),
-		.pt_rddata(pt_rddata),
-		.pt_wrdata(pt_wrdata),
-		.pt_wren(pt_wren));
+		arc4 a4(.clk(CLOCK_50),
+			.rst_n(rst_n),
+			.en(en),
+			.rdy(rdy),
+			.key(key),
+			.ct_addr(ct_addr),
+			.ct_rddata(ct_rddata),
+			.pt_addr(pt_addr),
+			.pt_rddata(pt_rddata),
+			.pt_wrdata(pt_wrdata),
+			.pt_wren(pt_wren));
 
-    // your code here
 
 endmodule: task3
 
