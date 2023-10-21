9992c42c58342de6fb7e0fd837f20ea3e2270338
Beluguy <hello@marcusc.me>
2023-10-20 17:39:38 -0700

    updated to submit

--- 3/ToSubmit/Task3/tb_syn_arc4.sv
+++ 3/ToSubmit/Task3/tb_syn_arc4.sv
@@ -1,66 +1,101 @@
 `timescale 1 ps / 1 ps
 module tb_rtl_arc4();
+	integer failed = 0;
+	integer i = 0;
 
-integer failed = 0;
-integer i = 0;
+/*module arc4(input logic clk, input logic rst_n,
+            input logic en, output logic rdy,
+            input logic [23:0] key,
+            output logic [7:0] ct_addr, input logic [7:0] ct_rddata,
+            output logic [7:0] pt_addr, input logic [7:0] pt_rddata, output logic [7:0] pt_wrdata, output logic pt_wren)*/
 
-reg clk;
-reg rst_n;
-reg en;
-wire rdy;
-reg [23:0] key;
-wire [7:0] ct_addr;
-reg [7:0] ct_rddata;
-wire [7:0] pt_addr;
-reg [7:0] pt_rddata;
-wire [7:0] pt_wrdata;
-wire pt_wren;
+	reg clk, rst_n, en;
+	wire rdy, pt_wren;
+	reg [23:0] key;
+	wire [7:0] ct_addr;
+	reg [7:0] ct_rddata;
+	wire [7:0] pt_addr;
+	reg [7:0] pt_rddata;
+	wire [7:0] pt_wrdata;
 
-arc4 dut(.clk(clk),
-	.rst_n(rst_n),
-	.en(en),
-	.rdy(rdy),
-	.key(key),
-	.ct_addr(ct_addr),
-	.ct_rddata(ct_rddata),
-	.pt_addr(pt_addr),
-	.pt_rddata(pt_rddata),
-	.pt_wrdata(pt_wrdata),
-	.pt_wren(pt_wren));
+	arc4 dut(.clk(clk),
+		.rst_n(rst_n),
+		.en(en),
+		.rdy(rdy),
+		.key(key),
+		.ct_addr(ct_addr),
+		.ct_rddata(ct_rddata),
+		.pt_addr(pt_addr),
+		.pt_rddata(pt_rddata),
+		.pt_wrdata(pt_wrdata),
+		.pt_wren(pt_wren));
 
-integer count;
+	task clock;
+		begin
+			clk = 1'b1;
+			#1;
+			clk = 1'b0;
+			#1;
+		end
+	endtask
 
-task clock;
-	begin
-		clk = 1'b1;
+
+	initial begin
+		rst_n = 1'b0; //press reset
+		#1;       
+		rst_n = 1'b1;
 		#1;
-		clk = 1'b0;
-		#1;
-	end
-endtask
-
-
-initial begin
-	rst_n = 1'b0;
-	#1;       
-	rst_n = 1'b1;
-	count = 0;
-	force dut.p.ct_length = 7'd100;
-	key = 10'b1100111100;
-	#1;
-	clock;
-	clock;
-	clock;
-	#1;
-	en = 1'b1;
-	#1;
-	clock;
-	for(i = 0; i < 3073; i = i + 1) begin  //after 256 clk cycles init is done, after 3072 cycles ksa is done
-		count = count + 1;
+		clock;
+		en = 1'b1; //press enable
+		clock;
 		en = 1'b0;
 		clock;
+		if(rdy == 1'b0) $display("Correct: rdy is 0 after reset and enable");
+		else begin
+			$error("Incorret: rdy is %d after reset and enable", rdy);
+			failed = failed + 1;	
+		end
+
+		//testing the always comb
+		force dut.init_started = 1;
+		force dut.init_done = 1;
+		clock;
+		if(dut.ksa_en_reg == 1) $display("Correct: ksa_en_reg is 1 after reset and enable");
+		else begin
+			$error("Incorret: ksa_en_reg is %d after reset and enable", dut.ksa_en_reg);
+			failed = failed + 1;	
+		end
+		
+		force dut.ksa_started = 1;
+		force dut.ksa_done = 1;
+		clock;
+		if(dut.prga_en_reg == 1) $display("Correct: prga_en_reg is 1 after reset and enable");
+		else begin
+			$error("Incorret: prga_en_reg is %d after reset and enable", dut.prga_en_reg);
+			failed = failed + 1;	
+		end
+
+
+		//testing the always block
+		force dut.state = 0;
+		force dut.en = 1;
+		clock;
+		if(dut.init_started == 1) $display("Correct: prga_en_reg is 1 after reset and enable");
+		else begin
+			$error("Incorret: prga_en_reg is %d after reset and enable", dut.init_started);
+			failed = failed + 1;	
+		end
+
+		force dut.state = 1;
+		force dut.ksa_en = 1;
+		clock;
+		if(dut.ksa_started == 1) $display("Correct: init_started is 1 after reset and enable");
+		else begin
+			$error("Incorret: init_started is %d after reset and enable", rdy);
+			failed = failed + 1;	
+		end
+
+		$display("Tests failed: %d", failed);
+		$stop;
 	end
-
-end
-
 endmodule: tb_rtl_arc4
