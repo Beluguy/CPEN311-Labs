a1424e8087ba549066b947a1970e2ef25a5009e7
NissanFairladyZ <117058399+NissanFairladyZ@users.noreply.github.com>
2023-11-02 20:16:29 -0700

    Add files via upload

--- 4/task4/task4.sv
+++ 4/task4/task4.sv
@@ -9,4 +9,44 @@ module task4(input logic CLOCK_50, input logic [3:0] KEY,
 
     // instantiate and connect the VGA adapter and your module
 
+logic [9:0] VGA_R_10;
+logic [9:0] VGA_G_10;
+logic [9:0] VGA_B_10;
+
+assign VGA_R = VGA_R_10[9:2];
+assign VGA_G = VGA_G_10[9:2];
+assign VGA_B = VGA_B_10[9:2];
+
+wire [2:0] colour;
+wire [7:0] x;
+wire [6:0] y;
+wire plot;
+wire done;
+integer centre_x = 80;
+integer centre_y = 60;
+integer diameter = 80;
+
+
+
+
+logic VGA_BLANK, VGA_SYNC;
+
+vga_adapter#(.RESOLUTION("160x120")) vga_u0(.resetn(KEY[3]), .clock(CLOCK_50), .colour(colour),
+                                            .x(x), .y(y), .plot(plot),
+                                            .VGA_R(VGA_R_10), .VGA_G(VGA_G_10), .VGA_B(VGA_B_10),
+                                            .*);
+
+reuleaux rotary(.clk(CLOCK_50),
+		.rst_n(KEY[3]),
+		.colour(colour),
+		.centre_x(centre_x),
+		.centre_y(centre_y),
+		.diameter(diameter),
+		.start(~KEY[0]),
+             .done(done),
+             .vga_x(x),
+             .vga_y(y),
+             .vga_colour(colour),
+	     .vga_plot(plot));
+
 endmodule: task4
--- 4/task4/tb_rtl_task4.sv
+++ 4/task4/tb_rtl_task4.sv
@@ -1,5 +1,56 @@
+`timescale 1 ps / 1 ps
 module tb_rtl_task4();
 
 // Your testbench goes here. Our toplevel will give up after 1,000,000 ticks.
+// Your testbench goes here. Our toplevel will give up after 1,000,000 ticks.
+ logic CLK;
+    logic [3:0] KEY;
+    logic [9:0] SW;
+    logic [9:0] LEDR;
+    logic [6:0] HEX0;
+    logic [6:0] HEX1;
+    logic [6:0] HEX2;
+    logic [6:0] HEX3;
+    logic [6:0] HEX4;
+    logic [6:0] HEX5;
+
+    de1_gui gui(.SW, .KEY, .LEDR, .HEX5, .HEX4, .HEX3, .HEX2, .HEX1, .HEX0);
+   
+    task4 dut(.CLOCK_50(CLK),
+		.KEY(KEY));
+
+	task clk;
+		begin
+			#1;
+			CLK = 1;
+			#1;
+			CLK = 0;
+			#1;
+		end
+	endtask
+
+integer count = 0;
+
+initial begin
+KEY = 4'b1111;
+clk;
+KEY[3] = 1'b0;
+#1;
+KEY[3] = 1'b1;
+clk;
+KEY[0] = 1'b0;
+clk;
+KEY[0] = 1'b1;
+for(count = 0; count < 19210; count = count + 1) begin
+	clk;
+end
+for(count = 0; count < 500; count = count + 1) begin
+	clk;
+end
+ $stop;
+
+
+
+end
 
 endmodule: tb_rtl_task4
