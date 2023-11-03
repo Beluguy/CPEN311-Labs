ad0577f7434dda1a2a6589448baa7732dba270f3
NissanFairladyZ <117058399+NissanFairladyZ@users.noreply.github.com>
2023-11-03 09:41:28 -0700

    Update task3.sv

--- 4/task3/task3.sv
+++ 4/task3/task3.sv
@@ -26,6 +26,12 @@ integer centre_x = 80;
 integer centre_y = 60;
 integer radius = 40;
 
+assign VGA_X = x;
+assign VGA_Y = y;
+assign VGA_COLOUR = colour;
+assign VGA_PLOT = plot;
+// ask someone what to output to VGA_HS and VGA_VS
+
 
 
 
